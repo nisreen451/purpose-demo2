e31b978bfdd7bf2390b24041ea32dfdbb33e77d9
nisreen451 <nisreenhisham50@gmail.com>
2023-03-15 10:16:10 +0200

    add python file

---
+++ Test-demo.py
@@ -0,0 +1,9 @@
+#detect light level
+from microbit import *
+
+while True:
+    if display.read_light_level() < 150:
+        display.scroll('Night')
+    else:
+        display.scroll('Day')
+    sleep(2000)
